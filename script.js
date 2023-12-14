89c5c94fcbf326874bf9ccbbe92f87615fa1e1e7
PawanSirsat <p1.sirsat1998@gmail.com>
2023-12-14 22:43:39 +0530

    Add New Section

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const itemsPerPage = 4;
 let currentPage = 1;
 
-function displayProjects(data) {
+function displayProjects1(data) {
   const projectList = document.getElementById('projectList');
   projectList.innerHTML = ''; // Clear existing content
 
@@ -35,11 +35,56 @@ function displayProjects(data) {
   });
 }
 
-function fetchDataAndDisplay() {
+function displayProjects2(data) {
+  const projectList = document.getElementById('projectList2');
+  projectList.innerHTML = ''; // Clear existing content
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const projectsToDisplay = data.slice(startIndex, endIndex);
+
+  projectsToDisplay.forEach((project) => {
+    const projectItem = document.createElement('li');
+    projectItem.className = 'project';
+    projectItem.innerHTML = `
+      <div class="project-info2">
+        <img id="bottom" src="${project.image}" alt="${project.name}">
+        <div class="newPadding">
+          <h3 id="zero-bottom">${project.name}</h3>
+          <p>${project.description}</p>
+          <div class="project-actions">
+            ${
+              project.liveDemo
+                ? `<a href="${project.liveDemo}" id="live" target="_blank">Live Demo</a>`
+                : `<button disabled>Live Demo</button>`
+            }
+            <a href="${
+              project.githubLink
+            }" target="_blank"><img width="30" height="30" src="https://img.icons8.com/glyph-neue/64/FFFFFF/github.png" alt="GitHub">Git Repo</a>
+          </div>
+        </div>
+      </div>
+    `;
+    projectList.appendChild(projectItem);
+  });
+}
+
+function fetchDataAndDisplay1() {
   fetch('projects.json')
     .then((response) => response.json())
     .then((data) => {
-      displayProjects(data);
+      displayProjects1(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+}
+
+function fetchDataAndDisplay2() {
+  fetch('projects2.json')
+    .then((response) => response.json())
+    .then((data) => {
+      displayProjects2(data);
     })
     .catch((error) => {
       console.error('Error fetching data:', error);
@@ -47,7 +92,8 @@ function fetchDataAndDisplay() {
 }
 
 // Fetch and display initial data
-fetchDataAndDisplay();
+fetchDataAndDisplay1();
+fetchDataAndDisplay2();
 
 // Example: Add event listeners for next and previous buttons
 document.getElementById('nextButton').addEventListener('click', () => {
