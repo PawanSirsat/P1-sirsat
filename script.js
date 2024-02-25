ffbdfba9c19c70fac02ef337b92971c8eca38e43
PawanSirsat <p1.sirsat1998@gmail.com>
2024-02-25 14:07:12 +0530

    Add new Projects

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-const itemsPerPage = 4;
-let currentPage1 = 1;
-let currentPage2 = 1;
+const itemsPerPage = 4
+let currentPage1 = 1
+let currentPage2 = 1
 
 function displayProjects1(data) {
-  const projectList = document.getElementById('projectList');
-  projectList.innerHTML = ''; // Clear existing content
+  const projectList = document.getElementById('projectList')
+  projectList.innerHTML = '' // Clear existing content
 
-  const startIndex = (currentPage1 - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const projectsToDisplay = data.slice(startIndex, endIndex);
+  const startIndex = (currentPage1 - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const projectsToDisplay = data.slice(startIndex, endIndex)
 
   projectsToDisplay.forEach((project) => {
-    const projectItem = document.createElement('li');
-    projectItem.className = 'project';
+    const projectItem = document.createElement('li')
+    projectItem.className = 'project'
     projectItem.innerHTML = `
       <div class="project-info">
         <img id="bottom" src="${project.image}" alt="${project.name}">
         <div class="newPadding">
           <h3 id="zero-bottom">${project.name}</h3>
+          <div id="tech">
+            ${project.tech
+              .map((tech) => `<img src="${tech}" alt="${tech}" />`)
+              .join('')}
+          </div>
           <p>${project.description}</p>
           <div class="project-actions">
             ${
@@ -31,31 +36,36 @@ function displayProjects1(data) {
           </div>
         </div>
       </div>
-    `;
-    projectList.appendChild(projectItem);
-  });
+    `
+    projectList.appendChild(projectItem)
+  })
 
   // Disable nextButton if there is no next page
-  const nextButton = document.getElementById('nextButton');
-  nextButton.disabled = endIndex >= data.length;
+  const nextButton = document.getElementById('nextButton')
+  nextButton.disabled = endIndex >= data.length
 }
 
 function displayProjects2(data) {
-  const projectList = document.getElementById('projectList2');
-  projectList.innerHTML = ''; // Clear existing content
+  const projectList = document.getElementById('projectList2')
+  projectList.innerHTML = '' // Clear existing content
 
-  const startIndex = (currentPage2 - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const projectsToDisplay = data.slice(startIndex, endIndex);
+  const startIndex = (currentPage2 - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const projectsToDisplay = data.slice(startIndex, endIndex)
 
   projectsToDisplay.forEach((project) => {
-    const projectItem = document.createElement('li');
-    projectItem.className = 'project';
+    const projectItem = document.createElement('li')
+    projectItem.className = 'project'
     projectItem.innerHTML = `
       <div class="project-info2">
         <img id="bottom" src="${project.image}" alt="${project.name}">
         <div class="newPadding">
           <h3 id="zero-bottom">${project.name}</h3>
+           <div id="tech">
+            ${project.tech
+              .map((tech) => `<img src="${tech}" alt="${tech}" />`)
+              .join('')}
+          </div>
           <p>${project.description}</p>
           <div class="project-actions">
             ${
@@ -69,62 +79,63 @@ function displayProjects2(data) {
           </div>
         </div>
       </div>
-    `;
-    projectList.appendChild(projectItem);
-  });
+    `
+    projectList.appendChild(projectItem)
+  })
 
   // Disable nextButton2 if there is no next page
-  const nextButton2 = document.getElementById('nextButton2');
-  nextButton2.disabled = endIndex >= data.length;
+  const nextButton2 = document.getElementById('nextButton2')
+  nextButton2.disabled = endIndex >= data.length
 }
 
 function fetchDataAndDisplay1() {
   fetch('projects.json')
     .then((response) => response.json())
     .then((data) => {
-      displayProjects1(data);
+      console.log(data)
+      displayProjects1(data)
     })
     .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
+      console.error('Error fetching data:', error)
+    })
 }
 
 function fetchDataAndDisplay2() {
   fetch('projects2.json')
     .then((response) => response.json())
     .then((data) => {
-      displayProjects2(data);
+      displayProjects2(data)
     })
     .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
+      console.error('Error fetching data:', error)
+    })
 }
 
 // Fetch and display initial data
-fetchDataAndDisplay1();
-fetchDataAndDisplay2();
+fetchDataAndDisplay1()
+fetchDataAndDisplay2()
 
 // Example: Add event listeners for next and previous buttons
 document.getElementById('nextButton').addEventListener('click', () => {
-  currentPage1++;
-  fetchDataAndDisplay1();
-});
+  currentPage1++
+  fetchDataAndDisplay1()
+})
 
 document.getElementById('prevButton').addEventListener('click', () => {
   if (currentPage1 > 1) {
-    currentPage1--;
-    fetchDataAndDisplay1();
+    currentPage1--
+    fetchDataAndDisplay1()
   }
-});
+})
 
 document.getElementById('nextButton2').addEventListener('click', () => {
-  currentPage2++;
-  fetchDataAndDisplay2();
-});
+  currentPage2++
+  fetchDataAndDisplay2()
+})
 
 document.getElementById('prevButton2').addEventListener('click', () => {
   if (currentPage2 > 1) {
-    currentPage2--;
-    fetchDataAndDisplay2();
+    currentPage2--
+    fetchDataAndDisplay2()
   }
-});
+})
